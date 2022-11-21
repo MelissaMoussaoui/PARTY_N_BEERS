1bfb124c7ec2ff9b3d3b263fb1a5414b934d0d52
Nicolas.N <nicolasnguyen0023@hotmail.com>
2022-11-21 15:40:24 +0100

    les routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "articles#index"end
+
+  resources :party_flats do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: :destroy
+  resources :users
 end
