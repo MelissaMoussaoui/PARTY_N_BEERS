8a7c50d52bade56d034a473da09f15164cddcc67
Nicolas.N <nicolasnguyen0023@hotmail.com>
2022-11-22 14:36:17 +0100

    Nico

--- app/controllers/party_flats_controller.rb
+++ app/controllers/party_flats_controller.rb
@@ -1,7 +1,6 @@
 class PartyFlatsController < ApplicationController
   before_action :set_party_flat, only: [:show, :edit, :update, :destroy]
 
-<<<<<<< HEAD
   def index
     @party_flats = policy_scope(PartyFlat)
   end
@@ -18,14 +17,11 @@ class PartyFlatsController < ApplicationController
       redirect_to party_flats_path
     else
       render :new, status: :unprocessable_entity
-=======
     def index
         @party_flats = policy_scope(PartyFlat)
->>>>>>> a162a2fd40c8f710f7fea09b9d7a28e65964b94c
     end
   end
 
-<<<<<<< HEAD
   def show
     authorize @party_flat
   end
@@ -40,11 +36,9 @@ class PartyFlatsController < ApplicationController
       redirect_to party_flat_path(@party_flat)
     else
       render :new, status: :unprocessable_entity
-=======
     def new
         @party_flat = PartyFlat.new
         authorize @party_flat
->>>>>>> a162a2fd40c8f710f7fea09b9d7a28e65964b94c
     end
   end
 
@@ -63,4 +57,3 @@ class PartyFlatsController < ApplicationController
   def set_party_flat
     @party_flat = PartyFlat.find(params[:id])
   end
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-<<<<<<< HEAD
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
-  # validates :gender, inclusion: {in: %w(F M IEL),
-  #   message: "%{value} is not a valid gender"}
-  # validates :phone_number, presence: true #format:
-  # validates :date_of_birth, presence: true
-=======
-#  validates :first_name, presence: true
- # validates :last_name, presence: true
-  #validates :gender, inclusion: {in: %w(F M IEL),
-   # message: "%{value} is not a valid gender"}
- # validates :phone_number, presence: true #format: { with: /^(?:(?:(?:\+|00)33[ ]?(?:\(0\)[ ]?)?)|0){1}[1-9]{1}([ .-]?)(?:\d{2}\1?){3}\d{2}$/ }
-  #validates :date_of_birth, presence: true 
->>>>>>> a162a2fd40c8f710f7fea09b9d7a28e65964b94c
 end
