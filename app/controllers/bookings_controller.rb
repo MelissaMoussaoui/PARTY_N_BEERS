0881f2503e97207be4119be529630dbacebdd781
Melissa Moussaoui <moussaoui.melissa@gmail.com>
2022-11-23 16:10:41 +0100

    booking done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,19 @@
 class BookingsController < ApplicationController
-    before_action :set_booking, only: [:new, :create, :destroy]
+    before_action :set_booking, only: [:destroy]
     before_action :find_party_flat, only: [:new, :create]
+    before_action :authenticate_user!
 
     def new
         @booking = Booking.new
+        authorize @booking
     end
 
     def create
         @booking = Booking.new(booking_params)
+        @booking.party_flat = @party_flat
+        authorize @booking
+        @booking.save
+        redirect_to party_flat_path(party_flat)
     end
 
     def destroy
@@ -18,7 +24,7 @@ class BookingsController < ApplicationController
     private
 
     def booking_params
-        params.require(:booking).permit(:start_date, :end_date, :id_partys, :id_users)
+        params.require(:booking).permit(:start_date, :end_date, :party_flat_id, :user_id)
     end
 
     def set_booking
@@ -26,7 +32,7 @@ class BookingsController < ApplicationController
     end
 
     def find_party_flat
-        @party_flat = Party_flat.find(params[:id])
+        @party_flat = PartyFlat.find(params[:party_flat_id])
     end
 
 end
