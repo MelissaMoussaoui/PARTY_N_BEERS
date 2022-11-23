e4f806d1a246904ede45953d639d48e5572152fd
Melissa Moussaoui <moussaoui.melissa@gmail.com>
2022-11-23 10:19:23 +0100

    map

--- app/models/party_flat.rb
+++ app/models/party_flat.rb
@@ -2,5 +2,6 @@ class PartyFlat < ApplicationRecord
     belongs_to :user
     has_many :bookings
 
-    
+    geocoded_by :localisation
+    after_validation :geocode, if: :will_save_change_to_localisation?
 end
